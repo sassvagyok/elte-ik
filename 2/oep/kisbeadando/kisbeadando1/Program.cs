032808930e16573f30027b569215a42e73634b0f
Pőcze Máté <76638318+sassvagyok@users.noreply.github.com>
2025-02-28 20:44:23 +0100

    cleanup

---
+++ 2/oep/kisbeadando/kisbeadando1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Base
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adagoló maximum tartalma, adag mennyisége és megnyomások száma:");
+            string[] input = Console.ReadLine().Split();
+            double max = double.Parse(input[0]);
+            double dose = double.Parse(input[1]);
+            int presses = int.Parse(input[2]);
+            Dispenser test = new Dispenser(max, dose);
+            Console.WriteLine(test.IsEmpty());
+            test.Fill();
+            Console.WriteLine(test.IsEmpty());
+            for(int i = 0; i < presses; i++)
+            {
+                test.Push();
+            }
+            Console.WriteLine(test.IsEmpty());
+        }
+    }
+}

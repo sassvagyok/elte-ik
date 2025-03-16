ea8eedf7263235190e48b92368f6a9598f9a4443
Pőcze Máté <76638318+sassvagyok@users.noreply.github.com>
2025-03-16 13:03:11 +0100

    oep kisbeadando3

---
+++ 2/oep/kisbeadando/kisbeadando3/Diag.cs
@@ -0,0 +1,102 @@
+﻿namespace HF3
+{
+    public class Diag
+    {
+        private double[] x;
+
+        public Diag(int n)
+        {
+            x = new double[n];
+        }
+
+        public double Get(int i, int j)
+        {
+            if (i < 0 || i >= x.Length || j < 0 || j >= x.Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            if (i == j) return x[i];
+            else return 0.0;
+        }
+
+        public void Set(int i, int j, double e)
+        {
+            if (i < 0 || i >= x.Length || j < 0 || j >= x.Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            
+            if(i == j) x[i] = e;
+            else throw new IndexOutOfRangeException();
+        }
+
+        public static Diag Add(Diag a, Diag b)
+        {
+            if (a.x.Length != b.x.Length)
+            {
+                throw new ArgumentException();
+            }
+
+            Diag c = new Diag(a.x.Length);
+
+            for (int i = 0; i < a.x.Length; i++)
+            {
+                c.x[i] = a.x[i] + b.x[i];
+            }
+
+            return c;
+        }
+
+        public static Diag Mul(Diag a, Diag b)
+        {
+            if (a.x.Length != b.x.Length)
+            {
+                throw new ArgumentException();
+            }
+            
+            Diag c = new Diag(a.x.Length);
+            
+            for (int i = 0; i < a.x.Length; i++)
+            {
+                c.x[i] = a.x[i] * b.x[i];
+            }
+
+            return c;
+        }
+        
+        public static Diag operator +(Diag a, Diag b)
+        {
+            if (a.x.Length != b.x.Length)
+            {
+                throw new ArgumentException();
+            }
+
+            Diag c = new Diag(a.x.Length);
+
+            for (int i = 0; i < a.x.Length; i++)
+            {
+                c.x[i] = a.x[i] + b.x[i];
+            }
+
+            return c;
+        }
+        
+        public static Diag operator *(Diag a, Diag b)
+        {
+            if (a.x.Length != b.x.Length)
+            {
+                throw new ArgumentException();
+            }
+            
+            Diag c = new Diag(a.x.Length);
+            
+            for (int i = 0; i < a.x.Length; i++)
+            {
+                c.x[i] = a.x[i] * b.x[i];
+            }
+
+            return c;
+        }
+    }
+}
---
+++ 2/oep/kisbeadando/kisbeadando3/Program.cs
@@ -0,0 +1,164 @@
+﻿namespace HF3
+{
+    public class Program
+    {
+        static void CreateDiag()
+        {
+            int size = int.Parse(Console.ReadLine()!);
+            try
+            {
+                Diag a = new(size);
+                Console.WriteLine("Konstruktor - korrekt");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Konstruktor - hiba");
+            }
+        }
+        static void TestGet()
+        {
+            int size = int.Parse(Console.ReadLine()!);
+            try
+            {
+                Diag a = new(size);
+                int changes = int.Parse(Console.ReadLine()!);
+                string[] input;
+                for (int i = 0; i < changes; i++)
+                {
+                    input = Console.ReadLine()!.Split();
+                    a.Set(int.Parse(input[0]), int.Parse(input[1]), double.Parse(input[2]));
+                }
+                int gets = int.Parse(Console.ReadLine()!);
+                for (int i = 0; i < gets; i++)
+                {
+                    input = Console.ReadLine()!.Split();
+                    Console.WriteLine($"{a.Get(int.Parse(input[0]), int.Parse(input[1]))}");
+                }
+                Console.WriteLine("GetTest vége");
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Get hiba");
+            }
+        }
+        static void TestSet()
+        {
+            int size = int.Parse(Console.ReadLine()!);
+            try
+            {
+                Diag a = new(size);
+                int changes = int.Parse(Console.ReadLine()!);
+                string[] input;
+                for (int i = 0; i < changes; i++)
+                {
+                    input = Console.ReadLine()!.Split();
+                    a.Set(int.Parse(input[0]), int.Parse(input[1]), double.Parse(input[2]));
+                    Console.WriteLine($"{a.Get(int.Parse(input[0]), int.Parse(input[1]))}");
+                }
+                Console.WriteLine("SetTest vége");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Set hiba");
+            }
+        }
+        static void TestAdd()
+        {
+            int size;
+            int changes;
+            string[] input;
+            size = int.Parse(Console.ReadLine()!);
+            Diag a = new(size);
+            changes = int.Parse(Console.ReadLine()!);
+            for (int i = 0; i < changes; i++)
+            {
+                input = Console.ReadLine()!.Split();
+                a.Set(int.Parse(input[0]), int.Parse(input[1]), double.Parse(input[2]));
+            }
+            size = int.Parse(Console.ReadLine()!);
+            Diag b = new(size);
+            changes = int.Parse(Console.ReadLine()!);
+            for (int i = 0; i < changes; i++)
+            {
+                input = Console.ReadLine()!.Split();
+                b.Set(int.Parse(input[0]), int.Parse(input[1]), double.Parse(input[2]));
+            }
+            try
+            {
+                Diag c = Diag.Add(a, b);
+                Diag d = a + b;
+                int gets = int.Parse(Console.ReadLine()!);
+                for (int i = 0; i < gets; i++)
+                {
+                    input = Console.ReadLine()!.Split();
+                    Console.WriteLine($"{c.Get(int.Parse(input[0]), int.Parse(input[1]))}");
+                    Console.WriteLine($"{d.Get(int.Parse(input[0]), int.Parse(input[1]))}");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Összeadás hiba");
+            }
+        }
+        static void TestMultiply()
+        {
+            int size;
+            int changes;
+            string[] input;
+            size = int.Parse(Console.ReadLine()!);
+            Diag a = new(size);
+            changes = int.Parse(Console.ReadLine()!);
+            for (int i = 0; i < changes; i++)
+            {
+                input = Console.ReadLine()!.Split();
+                a.Set(int.Parse(input[0]), int.Parse(input[1]), double.Parse(input[2]));
+            }
+            size = int.Parse(Console.ReadLine()!);
+            Diag b = new(size);
+            changes = int.Parse(Console.ReadLine()!);
+            for (int i = 0; i < changes; i++)
+            {
+                input = Console.ReadLine()!.Split();
+                b.Set(int.Parse(input[0]), int.Parse(input[1]), double.Parse(input[2]));
+            }
+            try
+            {
+                Diag c = Diag.Mul(a, b);
+                Diag d = a * b;
+                int gets = int.Parse(Console.ReadLine()!);
+                for (int i = 0; i < gets; i++)
+                {
+                    input = Console.ReadLine()!.Split();
+                    Console.WriteLine($"{c.Get(int.Parse(input[0]), int.Parse(input[1]))}");
+                    Console.WriteLine($"{d.Get(int.Parse(input[0]), int.Parse(input[1]))}");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Szorzás hiba");
+            }
+        }
+        static void Main()
+        {
+            int choice = int.Parse(Console.ReadLine()!);
+            switch (choice)
+            {
+                case 0:
+                    CreateDiag();
+                    break;
+                case 1:
+                    TestGet();
+                    break;
+                case 2:
+                    TestSet();
+                    break;
+                case 3:
+                    TestAdd();
+                    break;
+                case 4:
+                    TestMultiply();
+                    break;
+            }
+        }
+    }
+}

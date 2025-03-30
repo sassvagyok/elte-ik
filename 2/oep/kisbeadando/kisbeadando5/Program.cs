da17164d98cb041c5eebb497a07db63111702ec1
Pőcze Máté <76638318+sassvagyok@users.noreply.github.com>
2025-03-30 21:14:41 +0200

    oep kisbeadando5

---
+++ 2/oep/kisbeadando/kisbeadando5/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace kisbeadando5;
+
+internal class Program
+{
+    static void Main(String[] args)
+    {
+        double sum = 0.0;
+        int db = 0;
+        
+        using StreamReader st = new StreamReader(args[0]);
+
+        if (st.EndOfStream) return;
+        
+        int e = int.Parse(st.ReadLine());
+
+        while (!st.EndOfStream && e >= 0)
+        {
+            sum += e;
+            db++;
+            
+            e = int.Parse(st.ReadLine());
+        }
+        
+        double átl = sum / db;
+
+        bool l = true;
+        int min = e;
+
+        while (!st.EndOfStream)
+        {
+            e = int.Parse(st.ReadLine());
+            
+            l = l && e < 0;
+
+            if (e < min)
+            {
+                min = e;
+            }
+        }
+        
+        Console.WriteLine(átl);
+        Console.WriteLine(l);
+        Console.WriteLine(min);
+    }
+}

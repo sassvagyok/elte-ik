0c49fcfe1cb39c320f4747c86485e745c5e5fd7c
Pőcze Máté <76638318+sassvagyok@users.noreply.github.com>
2025-10-07 22:10:38 +0200

    Update exercises04_studentFB.py

--- 3/python/gyakorlat/gyak04-1001/exercises04_studentFB.py
+++ 3/python/gyakorlat/gyak04-1001/exercises04_studentFB.py
@@ -19,28 +19,28 @@ square = lambda x : x ** 2
 print(square(4))  # Kimenet: 16
 
 # 6. Feladat: Lambda páros számok szűrésére egy listából
-filter_even = None #TODO
-#print(filter_even([1, 2, 3, 4, 5, 6]))  # Kimenet: [2, 4, 6]
+filter_even = lambda lst: [x for x in lst if x % 2 == 0]
+print(filter_even([1, 2, 3, 4, 5, 6]))  # Kimenet: [2, 4, 6]
 
 # 7. Feladat: Lambda stringek listájának nagybetűssé alakítására
-to_uppercase = None #TODO
-#print(to_uppercase(["hello", "world"]))  # Kimenet: ["HELLO", "WORLD"]
+to_uppercase = lambda lst: [x.upper() for x in lst]
+print(to_uppercase(["hello", "world"]))  # Kimenet: ["HELLO", "WORLD"]
 
 # 8. Feladat: Lambda tuple-ök listájának rendezésére a második elem alapján
 sort_by_second = lambda lst : sorted(lst, key=(lambda x : x[1]))
 print(sort_by_second([(1, 3), (2, 2), (3, 1)]))  # Kimenet: [(3, 1), (2, 2), (1, 3)]
 
 # 9. Feladat: Lambda stringek hosszának megtalálására egy listában
-lengths = None #TODO
-#print(lengths(["hello", "world"]))  # Kimenet: [5, 5]
+lengths = lambda lst : [len(x) for x in lst]
+print(lengths(["hello", "world"]))  # Kimenet: [5, 5]
 
 # 10. Feladat: Lambda egy konstans hozzáadására minden elemhez egy listában
-add_constant = None #TODO
-#print(add_constant([1, 2, 3], 5))  # Kimenet: [6, 7, 8]
+add_constant = lambda lst, c : [x + c for x in lst]
+print(add_constant([1, 2, 3], 5))  # Kimenet: [6, 7, 8]
 
 # 11. Feladat: Lambda két lista metszetének megtalálására
-intersection = None #TODO
-#print(intersection([1, 2, 3], [2, 3, 4]))  # Kimenet: [2, 3]
+intersection = lambda l1, l2 : [x for x in l1 if x in l2]
+print(intersection([1, 2, 3], [2, 3, 4]))  # Kimenet: [2, 3]
 
 # 12. Feladat: Lambda annak ellenőrzésére, hogy egy string csak számjegyeket tartalmaz-e
 is_digit = lambda x : x.isdigit()
